f097072e872b8ee8b8a0f398c27a786e8e952e83
uygnef <97508691@qq.com>
2016-08-08 12:43:35 +1000

    week2

---
+++ Lab/lab2/lab2.py
@@ -0,0 +1,26 @@
+from socket import *
+import sys, getopt
+import time
+
+ops, args = getopt.getopt(sys.argv[1:], " ")
+if len(args) != 2:
+    print("Input error")
+    exit()
+PORT = args[1]
+HOST = args[0]
+
+sock = socket(AF_INET, SOCK_STREAM)
+sock.connect((HOST, PORT))
+result = []
+for _ in range(10):
+    sock.send(''.encode("UTF-8"))
+    start_time = time.time()
+    sock.settimeout(1)
+    received = sock.recv(1024).encode("UTF-8")
+    end_time = time.time()
+    rtt = end_time - start_time
+    if received:
+        print("pint to {}, seq = {}, rtt = {}ms".format(HOST, _,rtt))
+    else:
+        print("NO Connection")
+
