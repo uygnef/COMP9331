bb5267e402dc747046e2bdab6726f2c1cc09567e
uygnef <97508691@qq.com>
2016-10-08 15:59:16 +1100

    node failure

--- assignment/ass2/Lsr.py
+++ assignment/ass2/Lsr.py
@@ -14,7 +14,7 @@ def decode(message):
     for lines in msg_list[1:]:
         data = lines.split(" ")
         if len(data) == 2:
-            graph[data[0]] = int(data[1])
+            graph[data[0]] = float(data[1])
     return node_name, graph
 
 argv = sys.argv[1:]
@@ -27,6 +27,7 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind(address)
 graph = {}
 node_port = {}
+keep_alive ={}
 
 # data: node_name port_number distance
 for lines in config_file:
@@ -34,8 +35,10 @@ for lines in config_file:
     if not node_name in graph:
         graph[node_name] = {}
     if len(data) == 3:
-        graph[node_name][data[0]] = int(data[1])
+        graph[node_name][data[0]] = float(data[1])
         node_port[data[0]] = int(data[2])
+        keep_alive[int(data[2])] = 3
+print(keep_alive)
 
 '''
 the recv_from is a dictionary.
@@ -47,17 +50,19 @@ def update_graph():
     global recv_from
     global graph
     inf, outf, errf = select.select([sock, ], [], [], 0)
+    global keep_alive
     if inf:
         for sockets in inf:
             message, ADDR = sockets.recvfrom(1024)
             node, recv_graph = decode(message)
             # if node_name == 'E' or ADDR[1] == 2002:
             #     print("recv node and graph are ", node,recv_graph)
+            keep_alive[ADDR[1]] = 3
             if node not in graph:
                 graph[node] = recv_graph
             if node not in recv_from:
                 recv_from[node] = [ADDR[1]]
-            else:
+            elif ADDR[1] not in recv_from[node]:
                 recv_from[node].append(ADDR[1])
 
 #def timer(port, node, send_graph)
@@ -69,25 +74,6 @@ def broadcast(node):
             #     print("send node is C to A", node)
             #     print("recv_form is", recv_from)
             sock.sendto(encode(node, graph[node]), ("localhost", port))
-
-recv_from = {node_name:[port_number]}
-# print("Initial recv_from is", recv_from)
-start_time = time.time()
-now_time = time.time() - 1 #make sure it will start immediately at beginning
-# while(time.time() <= start_time + 30):
-#     if time.time() >= now_time + 1:
-#         for node in graph:
-#             # if node_name == 'W':
-#             #     print(node)
-#             broadcast(node)
-#         update_graph()
-#         now_time = time.time()
-#         # if node_name == 'W':
-#         #     print("ccccc",node_name, graph)
-# print(node_name, graph)
-sock.close()
-
-graph = {'A': {'B': 2, 'D': 1, 'C': 5}, 'E': {'D': 1, 'F': 2, 'C': 1}, 'C': {'A': 5, 'E': 1, 'D': 3, 'F': 5, 'B': 3}, 'D': {'A': 1, 'C': 3, 'B': 2, 'E': 1}, 'F': {'C': 5, 'E': 2}, 'B': {'A': 2, 'D': 2, 'C': 3}}
 def Dijkstra_Algrm(graph):
     result_set = {}
     set_N = []
@@ -121,8 +107,85 @@ def Dijkstra_Algrm(graph):
                 if break_flag:
                     break
         sorted_result_set = sorted(result_set.items(), key=lambda d: d[1])
-
+    return result_set, from_to_list
     # print(set_N, sorted_result_set,from_to_list)
     # print(node_name, result_set)
 
-Dijkstra_Algrm(graph)
+def delete_node(node):
+    global node_port
+    global graph
+    global recv_from
+    port = node_port[node]
+    try:
+        del node_port[node]
+        del graph[node]
+        del recv_from[node]
+    except KeyError:
+        pass
+    for i in graph:
+        for j in graph[i]:
+            if j == node:
+                del graph[i][j]
+                break
+    for i in recv_from:
+        try:
+            recv_from[i].remove(port)
+        except ValueError:
+            pass
+
+recv_from = {node_name:[port_number]}
+# print("Initial recv_from is", recv_from)
+start_time = time.time()
+now_time = time.time() - 1 #make sure it will start immediately at beginning
+delete_list = []
+while(time.time() <= start_time + 30):
+    if node_name == 'D' and time.time() >= start_time +15:
+        break
+    if time.time() >= now_time + 1:
+        for node in graph:
+            # if node_name == 'W':
+            #     print(node)
+            broadcast(node)
+        update_graph()
+        #dealing with node failures
+        for port in node_port:
+            keep_alive[node_port[port]] -= 1
+            if keep_alive[node_port[port]] <= 0:
+                delete_list.append(port)
+        while delete_list:
+            delete_node(delete_list.pop())
+        now_time = time.time()
+        # if node_name == 'W':
+        #     print("ccccc",node_name, graph)
+print(node_name, graph)
+sock.close()
+
+graph = {'A': {'B': 2, 'D': 1, 'C': 5},
+         'E': {'D': 1, 'F': 2, 'C': 1},
+         'C': {'A': 5, 'E': 1, 'D': 3, 'F': 5, 'B': 3},
+         'D': {'A': 1, 'C': 3, 'B': 2, 'E': 1},
+         'F': {'C': 5, 'E': 2},
+         'B': {'A': 2, 'D': 2, 'C': 3}}
+
+def find_shortest_path(result_set, from_to_list):
+    #result set A {'F': 4, 'C': 3, 'D': 1, 'E': 2, 'B': 2}
+    #from_to_list {'F': 'E', 'C': 'E', 'D': 'A', 'E': 'D', 'B': 'A'}
+    least_cost = {}
+    for node in result_set:
+        least_cost[node] = [node]
+        next_node = from_to_list[node]
+        while next_node != node_name:
+            least_cost[node].append(next_node)
+            next_node = from_to_list[next_node]
+        least_cost[node].append(next_node)
+        least_cost[node].reverse()
+    for node in sorted(least_cost.keys()):
+        path = ""
+        for i in least_cost[node]:
+            path += str(i)
+        print("least-cost path to node %s: %s and the cost is %.1f"%(node, path, result_set[node]))
+    return least_cost
+
+
+result_set, from_to_list = Dijkstra_Algrm(graph)
+least_cost_path = find_shortest_path(result_set, from_to_list)
